981905e224666196bd5aa5543a0527e2fd4eecb1
Angel Gallego <angelgb1990@gmail.com>
2024-10-11 18:21:23 +0000

    creada rama para las primeras 4

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,18 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name != null ? variables.name : "Introduce nombre"} ${
+    variables.lastName != null ? variables.lastName : "Introduce apellido"
+  }</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          </ul>
+          <ul class="position-left">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
